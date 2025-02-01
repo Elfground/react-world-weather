9b06fab5ef717c80691c6c5b03593d8c6204176a
Elfground <elfground@gmail.com>
2025-02-01 15:31:03 +0100

    Removed unnecessary files and content. Added link

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>World Weather App</h1>
+    <footer>This project was coded by Monica Alvmarken and is open-sourced on <a href="https://www.github.com/Elfground" target="_blank">GitHub</a></footer>
+    </div>
+  );
+}
+
+export default App;
