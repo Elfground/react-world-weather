1e7d5964b7df2d344dcde60524191795d22b3e5c
Elfground <elfground@gmail.com>
2025-02-01 16:22:53 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <div className="container">
       <h1>World Weather App</h1>
     <footer>This project was coded by Monica Alvmarken and is open-sourced on <a href="https://github.com/Elfground/react-world-weather" target="_blank">GitHub</a></footer>
+    </div>
     </div>
   );
 }
