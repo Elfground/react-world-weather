051b63a8a1b76f68594004d3d90d2a2e4943ef37
Elfground <elfground@gmail.com>
2025-02-02 13:28:20 +0100

    Added API integration

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
     <div className="container">
       <h1>World Weather App</h1>
-      <Weather />
+      <Weather defaultCity="Falun" />
     <footer>This project was coded by Monica Alvmarken and is open-sourced on <a href="https://github.com/Elfground/react-world-weather" target="_blank" rel="noreferrer">GitHub</a></footer>
     </div>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-    return <div className="Weather">
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ ready: false });
+
+    function handleResponse(response) {
+        console.log(response.data);
+        setWeatherData({
+            ready: true,
+            city: response.data.city,
+            date: "Wednesday 07:00",
+            description: response.data.condition.description,
+            icon: response.data.condition.icon_url,
+            temperature: response.data.temperature.current,
+            feels_like: response.data.temperature.feels_like,
+            humidity: response.data.temperature.humidity,
+            wind: response.data.wind.speed,
+        });
+    }
+
+    if (weatherData.ready) {
+    return (
+         <div className="Weather">
         <form>
             <div className="row">
                 <div className="col-9">
@@ -13,31 +33,37 @@ export default function Weather() {
             </div>
             </div>
         </form>
-        <h1>Falun</h1>
+        <h1>{weatherData.city}</h1>
         <ul>
-            <li>
-                Wednesday 07:00
-            </li>
-            <li>Mostly cloudy</li>
+            <li>{weatherData.date}</li>
+            <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
             <div className="col-6">
                 <div className="clearfix">
-                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly cloud" className="float-left" />
+                <img src={weatherData.icon} alt={weatherData.description} className="float-left" />
                 <div className="float-left">
-                <span className="temperature">6</span>
+                <span className="temperature">{Math.round(weatherData.temperature)}</span>
                 <span className="unit">°C</span>
                 </div>
                 </div>
             </div>
             <div className="col-6">
                 <ul>
-                    <li>Precipitation: 15%</li>
-                    <li>Humidity: 72%</li>
-                    <li>Wind: 13 km/h</li>
+                    <li>Feels like: {Math.round(weatherData.feels_like)}</li>
+                    <li>Humidity: {weatherData.humidity}%</li>
+                    <li>Wind: {weatherData.wind} km/h</li>
                 </ul>
             
             </div>
         </div>
     </div>
+    );
+} else {
+    const apiKey = "2f78437a500ef24fc3e9894233eftb0o";
+    const unit ="metric";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=${unit}`;
+    axios.get(apiUrl).then(handleResponse)
+    return "Loading ...";
+}
 }
