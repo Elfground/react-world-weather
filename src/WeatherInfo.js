9ce4c5f46e139959f0aa0bcb02bda4a33f8583f1
Elfground <elfground@gmail.com>
2025-02-08 18:53:28 +0100

    Updated CSS

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -24,7 +24,7 @@ export default function WeatherInfo(props) {
                 </div>
             </div>
             <div className="col-6">
-                <ul>
+                <ul className="conditions">
                     <li>Feels like: {Math.round(props.data.feels_like)}°C</li>
                     <li>Humidity: {props.data.humidity}%</li>
                     <li>Wind: {props.data.wind} km/h</li>
